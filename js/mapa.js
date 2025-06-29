d15bdb7a2c7a41db10acf4d3b09795f670133158
Geshura <geshura@gmail.com>
2025-06-29 22:58:23 +0200

    3.7

--- js/mapa.js
+++ js/mapa.js
@@ -1,5 +1,8 @@
 document.querySelectorAll('.marker').forEach(marker => {
   marker.addEventListener('click', () => {
-    alert(`Kliknięto: ${marker.title}`);
+    const url = marker.dataset.url;
+    if (url) {
+      window.open(url, '_blank');  // otwiera link w nowej karcie
+    }
   });
 });
